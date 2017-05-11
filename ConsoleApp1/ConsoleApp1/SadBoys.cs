93fae9e833c40373c962c85cf6fa9c959100b373
Randy McClure <rmcclure@student.neumont.edu>
2017-05-10 18:18:05 -0600

    Created Cry method, and randy int

--- ConsoleApp1/ConsoleApp1/SadBoys.cs
+++ ConsoleApp1/ConsoleApp1/SadBoys.cs
@@ -9,5 +9,11 @@ namespace ConsoleApp1
     class SadBoys
     {
         int cristian = 0;
+		int randy = 8;
+
+		private void Cry(int time)
+		{
+			//SadBoy cries for time (in seconds)
+		}
     }
 }
