f8b74568d081ab83d34e3eae7cb7905c40b1f3cd
bwalsh362 <bwalsh@student.neumont.edu>
2017-05-15 18:18:23 -0600

    Added measurement class

---
+++ KeithsFunFunPantry/KeithsFunFunPantry/Measurement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithsFunFunPantry
+{
+    public class Measurement
+    {
+        enum units { tsp, Tbsp, cup, lb, oz, fluid_oz, pint, quart, gallon, ml, liter, gram, kilogram, count};
+        float amount;
+        public units Units { get; set; }
+        public float Amount { get; set; }
+    }
+}
