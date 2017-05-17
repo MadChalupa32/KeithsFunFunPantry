47144305c6a0a3add3bbf4dfa9c48e763b7b3b83
twharton <ty89ty@gmail.com>
2017-05-17 17:59:24 -0600

    Recommitting after final merging, building, and running

---
+++ KeithsFunFunPantry/KeithsFunFunPantry/CS/RecipeBook.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KeithsFunFunPantry
+{
+    
+    //find way to serialize the RecipeBook, potentially inhereting a List<Recipe>       SINGLETON
+    [Serializable]
+    public class RecipeBook : List<Recipe>
+    {
+        private static RecipeBook instance;
+        private RecipeBook()
+        {
+            using (var file = File.Open("recipeBook.txt", FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    var recipesInTheFile = format.Deserialize(file);
+                    if (recipesInTheFile != null)
+                    {
+                        instance = (RecipeBook)recipesInTheFile;
+                        Recipes = instance.Recipes;
+                    }
+                }
+                catch (SerializationException e)
+                {
+
+                }
+            };
+        }
+        public static RecipeBook Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new RecipeBook();
+                }
+                return instance;
+            }
+        }
+        static IFormatter format = new BinaryFormatter();
+        private List<Recipe> recipes = new List<Recipe>();
+
+        public List<Recipe> Recipes
+        {
+            get { return recipes; }
+            set { recipes = value; }
+        }
+
+        
+
+        public void SaveRecipes()
+        {
+            try
+            {
+                if (Instance != null)
+                {
+                    using (FileStream file = File.Open("recipeBook.txt", FileMode.OpenOrCreate))
+                    {
+                        format.Serialize(file, Instance);
+                        file.Close();
+                    };
+
+                }
+                
+            }
+            catch ( Exception e) 
+                {
+                MessageBox.Show(e.ToString());
+                } 
+
+        }
+    }
+}
