36e37bf9800125af1a36b5074af2e1ec2b98992a
Randy McClure <rmcclure@student.neumont.edu>
2017-05-23 18:38:52 -0600

    Created skeleton of the UnitConversion class with an example method. Also wrote daily commit note.

---
+++ KeithsFunFunPantry/ScratchConversions/UnitConversions/UnitConversion.cs
@@ -0,0 +1,37 @@
+﻿using KeithsFunFunPantry.CS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchConversions.UnitConversions
+{
+	public static class UnitConversion
+	{
+		public static void Convert(this Measurement original, Unit unit)
+		{
+			//Validation
+			//Conversion
+			//Change the original measurement
+		}
+
+		//Private methods for each conversion
+		#region Dry
+
+		#endregion
+		#region Liquid
+
+		#endregion
+		#region Metric
+
+		#endregion
+
+
+		private static int CupToTablespoon(int amount)
+		{
+			//Math to convert <amount> cups into <x> tablespoons
+			return amount;
+		}
+	}
+}
