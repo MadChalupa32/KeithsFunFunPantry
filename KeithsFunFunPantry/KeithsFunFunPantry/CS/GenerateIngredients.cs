753647177b4c247a41c821179a283750ce55608c
elizabeth0719 <tdudley@student.neumont.edu>
2017-06-02 18:01:53 -0600

    Working on generating ingredients

--- KeithsFunFunPantry/KeithsFunFunPantry/CS/GenerateIngredients.cs
+++ KeithsFunFunPantry/KeithsFunFunPantry/CS/GenerateIngredients.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KeithsFunFunPantry.CS
 {
-    //public class GenerateIngredients
-    //{
-        //Measurement measure = new Measurement(2f, Unit.Cup);
-        //List<Ingredient> ingredients = new List<Ingredient>{
-          // new Ingredient("Salt", measure),
-           //new Ingredient("Ground Beef", measure),
-           //new Ingredient("Oil", measure)
-        //};
-    //}
+    public class GenerateIngredients
+    {
+        Measurement measure;
+        public GenerateIngredients()
+        {
+           measure = new Measurement(2f, Unit.Cup);
+
+                new Ingredient("Salt", measure);
+                new Ingredient("Ground Beef", measure);
+                new Ingredient("Oil", measure);
+            
+        }
+
+        public static ObservableCollection<Ingredient> Ingredients { get; set; }
+
+    }
 }
